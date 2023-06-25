51efa690352f080bedcb6f6ea53b756873d2b9fc
osdal <osdal67@gmail.com>
2023-06-25 12:27:13 +0300

    Подгнал вручную

---
+++ src/js/files/script.js
@@ -0,0 +1,6 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+let myName = $(".page__center h1").lettering();
